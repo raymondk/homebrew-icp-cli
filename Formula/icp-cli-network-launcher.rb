2384ee8a96c0a61b4a651091feb0bd1033ce003c
Adam Spofford <93943719+adamspofford-dfinity@users.noreply.github.com>
2025-12-17 01:09:03 +0000

    icp-cli-network-launcher: add 11.0.0_1 bottle.

--- Formula/icp-cli-network-launcher.rb
+++ Formula/icp-cli-network-launcher.rb
@@ -6,6 +6,13 @@ class IcpCliNetworkLauncher < Formula
   license "Apache-2.0"
   revision 1
 
+  bottle do
+    root_url "https://github.com/raymondk/homebrew-icp-cli/releases/download/icp-cli-network-launcher-11.0.0_1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37b02db9abd5c6123cb7b11687212a43cb47b936765284aba87df76a0dc50404"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d9ed66f91d89b82cedd78c342f1adcd1b5ef99da5bf25fddb23af46f7d01d891"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "db0a673d1eb0dced1b1fa9ff6209faae5aba11c76c284ce9973563866a85c403"
+  end
+
   depends_on "jq" => :build
   depends_on "rust" => :build
 
